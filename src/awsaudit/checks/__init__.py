42c10b4d51e61c360c47135b855a0cebb5e6ef4f
Bvlik <112949836+bvlik@users.noreply.github.com>
2026-06-22 08:53:55 +0200

    feat: register new checks in ALL_CHECKS (9 -> 15 checks)

--- src/awsaudit/checks/__init__.py
+++ src/awsaudit/checks/__init__.py
@@ -1,22 +1,34 @@
 """Check registry."""
 from .cloudtrail import CloudTrailLoggingCheck
 from .ebs_encryption import EbsEncryptionCheck
+from .ec2_imdsv2 import Ec2Imdsv2Check
+from .iam_admin import IamAdminPolicyCheck
 from .iam_key_age import IamAccessKeyAgeCheck
 from .iam_mfa import IamUserMfaCheck
 from .iam_password_policy import IamPasswordPolicyCheck
+from .iam_root_keys import IamRootKeysCheck
+from .kms_rotation import KmsRotationCheck
+from .rds_encryption import RdsEncryptionCheck
 from .rds_public import RdsPublicCheck
 from .s3_encryption import S3EncryptionCheck
 from .s3_public import S3PublicBucketsCheck
+from .s3_versioning import S3VersioningCheck
 from .security_groups import SecurityGroupOpenPortsCheck
 
 ALL_CHECKS = [
     SecurityGroupOpenPortsCheck(),
     S3PublicBucketsCheck(),
     S3EncryptionCheck(),
+    S3VersioningCheck(),
     IamUserMfaCheck(),
+    IamRootKeysCheck(),
+    IamAdminPolicyCheck(),
     IamAccessKeyAgeCheck(),
     IamPasswordPolicyCheck(),
     CloudTrailLoggingCheck(),
     RdsPublicCheck(),
+    RdsEncryptionCheck(),
     EbsEncryptionCheck(),
+    Ec2Imdsv2Check(),
+    KmsRotationCheck(),
 ]
